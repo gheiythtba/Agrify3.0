07bc2552c63bad9392fc763ba8c0104221f69677
Nessrine777 <nessrine.raies@gmail.com>
2023-11-11 22:38:48 +0100

    Creation Template +DB +entities+ Associations +CRUD + Forms

--- src/Controller/VenteController.php
+++ src/Controller/VenteController.php
@@ -8,7 +8,7 @@ use App\Repository\VenteRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Response; 
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/vente')]
