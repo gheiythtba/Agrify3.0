f1dc2fb2e2152222f3d4adce2203d6fd5cc8e245
Nessrine777 <nessrine.raies@gmail.com>
2023-11-13 23:59:42 +0100

    Creation choices in forms

--- src/Form/VenteType.php
+++ src/Form/VenteType.php
@@ -6,6 +6,8 @@ use App\Entity\Vente;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class VenteType extends AbstractType
 {
